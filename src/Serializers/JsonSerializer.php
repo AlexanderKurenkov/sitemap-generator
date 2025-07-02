12c4b18bae4d4c991c3362b0db4cca9b0cf534d5
Alexander Kurenkov <contact.kurenkov@gmail.com>
2025-07-02 20:45:35 +0700

    Исправлено название пространства имен для классов, выполняющих сериализацию.

--- src/Serializers/JsonSerializer.php
+++ src/Serializers/JsonSerializer.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace SitemapGenerator\Serializers;
+namespace Pyrobyte\SitemapGenerator\Serializers;
 
 use Pyrobyte\SitemapGenerator\Contracts\SerializerInterface;
 
